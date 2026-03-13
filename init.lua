f658a8977e30cc1c52cd0f40d752cc2605ea5a76
Ryan Cain <racain05@gmail.com>
2026-03-13 10:52:17 -0400

    fixing neo-tree

--- init.lua
+++ init.lua
@@ -247,8 +247,9 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'tpope/vim-fugitive',
-  'racain/jetbrains-one-dark-theme'
-  },
+  'racain/jetbrains-one-dark-theme',
+  'nvim-neo-tree/neo-tree.nvim',
+},
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
